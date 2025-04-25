9fc458d6b8b7b5d450cc1bcc47019499dfffbae7
Jan Smoczyński <jan.smoczynski.98@gmail.com>
2025-04-25 17:19:30 +0200

    Final PC Layout and Start for Hobby section

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
 const isMobile = window.matchMedia("(max-width: 768px)").matches; 
 const isTablet = window.matchMedia("(min-width: 769px) and (max-width: 1024px)").matches;
 const isPC = window.matchMedia("(min-width: 1025px)").matches;
-const windowHeight = window.innerHeight; // Get the height of the window in pixels
-
-const gridElem = document.querySelector(".grid"); // Select the grid element
-const gridHeight = windowHeight-100; // height minus padding`
 const deviceFlag = ()=>{if(isMobile){return "mobile"}else if(isTablet){return "tablet"}else if(isPC){return "pc"}} // Function to check the device type
 const deviceType = deviceFlag(); // Get the device type
+//width of the window in pixels
+const gridElem = document.querySelector(".grid"); // Select the grid element
+const windowHeight = window.innerHeight; // Get the height of the window in pixels
+const gridHeight = windowHeight; // height minus padding`
 
-gridElem.style.height=`${gridHeight}px`; // Set the height of the grid element to the window height minus 100 pixels
 
 
 if(deviceType === "mobile"){
